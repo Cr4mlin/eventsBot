a90279f36232fc270d1b8abeb457da75f076a8ef
Cr4mlin <Cramlin@yandex.ru>
2024-04-29 18:48:45 +0700

    Сделал просмотр

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ token = '6735334749:AAEgtyEKVTYdJEBxsIR8KXXnbRGuHEYZ2wQ'
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
+@bot.message_handler(content_types=['text'])
+def text(message):
     global us
     us = message.chat.username
     markup = types.InlineKeyboardMarkup(row_width=3)
@@ -18,6 +18,13 @@ def start(message):
     bot.send_message(message.chat.id, 'Выбери, действие', reply_markup=markup)
 
 
+def add(message):
+    bot.send_message(message.chat.id, 'Сделано!')
+    spisok = str(message.text).split()
+    print(spisok[2])
+    db.add_event(us, spisok[0], spisok[1], spisok[2])
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     global ci, mi
@@ -26,26 +33,16 @@ def callback(call):
     if call.message:
         if call.data == 'add':
             msg = bot.edit_message_text(chat_id=ci, message_id=mi,
-                                  text='Напиши дату, время и действие')
+                                        text='Напиши дату, время и действие')
             bot.register_next_step_handler(msg, add)
         elif call.data == 'view':
-            msg = bot.edit_message_text(chat_id=ci, message_id=mi,
-                                  text='Список ваших дел')
-            bot.register_next_step_handler(msg, view)
+            b = db.view_event(us)
+            s = ''
+            for elem in b:
+                a = elem['date'], elem['time'], elem['event']
+                s += ' -- '.join(a) + '\n'
+            bot.edit_message_text(chat_id=ci, message_id=mi,
+                                  text='Список ваших дел:\n' + s)
 
-@bot.message_handler(content_types=['text'])
-def text(message):
-    bot.send_message(message.chat.id, 'Вы что-то хотели?')
-def add(message):
-    bot.send_message(message.chat.id, 'Сделано!')
-    spisok = str(message.text).split()
-    print(spisok[2])
-    db.add_event(us, spisok[0], spisok[1], spisok[2])
-
-def view():
-    b = db.view_event(us)
-    # bot.edit_message_text(chat_id=ci, message_id=mi,
-    #                       text='Список ваших дел')
-    print(b)
 
 bot.infinity_polling()
