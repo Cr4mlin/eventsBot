3c0b75342246d46f15ba737c20ed501e8574c832
Cr4mlin <Cramlin@yandex.ru>
2024-04-29 17:39:21 +0700

    Скелет для просмотра

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    global us
+    us = message.chat.username
     markup = types.InlineKeyboardMarkup(row_width=3)
     item1 = types.InlineKeyboardButton('Добавить', callback_data='add')
     item2 = types.InlineKeyboardButton('Удалить', callback_data='del')
@@ -18,11 +20,18 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    global ci, mi
+    ci = call.message.chat.id
+    mi = call.message.id
     if call.message:
         if call.data == 'add':
-            msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+            msg = bot.edit_message_text(chat_id=ci, message_id=mi,
                                   text='Напиши дату, время и действие')
             bot.register_next_step_handler(msg, add)
+        elif call.data == 'view':
+            msg = bot.edit_message_text(chat_id=ci, message_id=mi,
+                                  text='Список ваших дел')
+            bot.register_next_step_handler(msg, view)
 
 @bot.message_handler(content_types=['text'])
 def text(message):
@@ -31,8 +40,12 @@ def add(message):
     bot.send_message(message.chat.id, 'Сделано!')
     spisok = str(message.text).split()
     print(spisok[2])
-    us = message.chat.username
     db.add_event(us, spisok[0], spisok[1], spisok[2])
 
+def view():
+    b = db.view_event(us)
+    # bot.edit_message_text(chat_id=ci, message_id=mi,
+    #                       text='Список ваших дел')
+    print(b)
 
 bot.infinity_polling()
