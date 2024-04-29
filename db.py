004c25ba94992a7eac16d30439b6ad47f001e286
Cr4mlin <Cramlin@yandex.ru>
2024-04-29 17:23:03 +0700

    Добавление работает

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import csv
 
 def add_event(userid, date, time, event):
-    with open('events.csv', 'w', newline='', encoding='utf-8') as f:
+    with open('events.csv', 'a', newline='\n', encoding='utf-8') as f:
         fieldnames = ['userid', 'date', 'time', 'event']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
         writer.writerow({'userid': userid, 'date': date, 'time': time, 'event': event})
