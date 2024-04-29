3da29a47197f4550e27cd5e9971432810f7cb16c
Cr4mlin <Cramlin@yandex.ru>
2024-04-29 16:24:54 +0700

    Почти сделал добавление

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import csv
 
 def add_event(userid, date, time, event):
-    with open('events.csv', 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    with open('events.csv', 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['userid', 'date', 'time', 'event']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
         writer.writerow({'userid': userid, 'date': date, 'time': time, 'event': event})
--- main.py
+++ main.py
@@ -5,22 +5,34 @@ from telebot import types
 token = '6735334749:AAEgtyEKVTYdJEBxsIR8KXXnbRGuHEYZ2wQ'
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Добавить')
-    item2 = types.KeyboardButton('Удалить')
-    item3 = types.KeyboardButton('Посмотреть')
+    markup = types.InlineKeyboardMarkup(row_width=3)
+    item1 = types.InlineKeyboardButton('Добавить', callback_data='add')
+    item2 = types.InlineKeyboardButton('Удалить', callback_data='del')
+    item3 = types.InlineKeyboardButton('Посмотреть', callback_data='view')
     markup.add(item1, item2, item3)
     bot.send_message(message.chat.id, 'Выбери, действие', reply_markup=markup)
-@bot.message_handler(content_types='text')
-def main(message):
-    if message.text == 'Добавить':
-        bot.send_message(message.chat.id, 'Напишите дату, время, событие')
 
-@bot.message_handler(content_types='text')
-def add_e(message):
-    date, time, event = message.text.split
-    db.add_event(message.chat.id, date, time, event)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.message:
+        if call.data == 'add':
+            msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                  text='Напиши дату, время и действие')
+            bot.register_next_step_handler(msg, add)
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot.send_message(message.chat.id, 'Вы что-то хотели?')
+def add(message):
+    bot.send_message(message.chat.id, 'Сделано!')
+    spisok = str(message.text).split()
+    print(spisok[2])
+    us = message.chat.username
+    db.add_event(us, spisok[0], spisok[1], spisok[2])
+
 
 bot.infinity_polling()
