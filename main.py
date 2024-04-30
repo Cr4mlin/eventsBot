f772d7ab1ca3803abbb22e2dcfc04eb4e72997b0
Cr4mlin <Cramlin@yandex.ru>
2024-04-30 19:19:19 +0700

    новое удаление

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ bot = telebot.TeleBot(token)
 # Регулярное выражение для проверки формата даты и времени
 date_time_pattern = re.compile(r'^(\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}) (.*)$')
 
+l = 8
+
 
 @bot.message_handler(content_types=['text'])
 def text(message):
@@ -33,17 +35,17 @@ def add(message):
                          'Формат ввода неправильный, попробуйте написать так - 1.05.2024 7:30 пробуждение')
 
 
-def delete(message):
-    # try:
-    number = int(message.text)
-    b = db.view_event(us)
-    b = sorted(b, key=lambda x: datetime.strptime(x['date'] + ' ' + x['time'], '%d.%m.%Y %H:%M'))
-    e = b[number - 1]['event']
-    d = b[number - 1]['date']
-    t = b[number - 1]['time']
-    db.del_event(us, e, d, t)
-    # except ValueError:
-    #     bot.send_message(message.chat.id, 'Пожалуйста, введите число')
+# def delete(message):
+#     try:
+#         number = int(message.text)
+#         b = db.view_event(us)
+#         b = sorted(b, key=lambda x: datetime.strptime(x['date'] + ' ' + x['time'], '%d.%m.%Y %H:%M'))
+#         e = b[number - 1]['event']
+#         d = b[number - 1]['date']
+#         t = b[number - 1]['time']
+#         db.del_event(us, e, d, t)
+#     except ValueError:
+#         bot.send_message(message.chat.id, 'Пожалуйста, введите число')
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -67,19 +69,46 @@ def callback(call):
                 s += ' -- '.join(a) + '\n'
             bot.edit_message_text(chat_id=ci, message_id=mi,
                                   text='Список ваших дел:\n' + s)
-        elif call.data == 'del':
+        elif call.data == 'del' or call.data == 'next':
             b = db.view_event(us)
             # Сортировка по времени
             b = sorted(b, key=lambda x: datetime.strptime(x['date'] + ' ' + x['time'], '%d.%m.%Y %H:%M'))
-            s = ''
-            c = 1
             for elem in b:
-                a = elem['date'], elem['time'], elem['event']
-                s += f'{str(c)}. ' + ' -- '.join(a) + '\n'
-                c += 1
-            msg = bot.edit_message_text(chat_id=ci, message_id=mi,
-                                        text='Напишите номер события, которое хотели бы удалить:\n' + s)
-            bot.register_next_step_handler(msg, delete)
+                markup1 = types.InlineKeyboardMarkup(row_width=len(b) + 1 if len(b) <= l else l)
+                if len(b) <= l:
+                    c = 1
+                elif len(b) // l != 0 and len(b) % l != 0:
+                    c = len(b) // l + 1
+                else:
+                    c = len(b) // l
+                for i in range(c):
+                    for elem in b:
+                        a = elem['date'], elem['time'], elem['event']
+                        markup1.add(types.InlineKeyboardButton(' -- '.join(a), callback_data=' '.join(a)))
+
+            bot.edit_message_text(chat_id=ci, message_id=mi,
+                                  text='Список ваших событий:\n', reply_markup=markup1)
+        elif 'yes' in call.data:
+            h = str(call.data)[4::].split(' -- ')
+            e = h[2]
+            t = h[1]
+            d = h[0]
+            print(h)
+            print(d)
+            db.del_event(us, e, d, t)
+            bot.edit_message_text(chat_id=ci, message_id=mi, text=f'Сделано')
+        elif call.data == 'no':
+            msg = bot.send_message(call.message.chat.id, 'Хорошо')
+            bot.register_next_step_handler(msg, text)
+        else:
+            a = str(call.data).split()
+            h = ' -- '.join(a)
+            choice = types.InlineKeyboardMarkup(row_width=2)
+            yes = types.InlineKeyboardButton('Да', callback_data=f'yes {h}')
+            no = types.InlineKeyboardButton('Нет', callback_data='no')
+            choice.add(yes, no)
+            bot.edit_message_text(chat_id=ci, message_id=mi,
+                                  text=f'Вы точно хотите удалить событие:\n {h}?', reply_markup=choice)
 
 
 bot.polling()
