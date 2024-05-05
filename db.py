9abcff3b724fbd596dd6301f96c89fe63bee3525
Cr4mlin <Cramlin@yandex.ru>
2024-05-05 17:57:17 +0700

    багфикс

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ def add_event(userid, date, time, event):
     with open('data/events.csv', 'a', newline='\n', encoding='utf-8') as f:
         fieldnames = ['userid', 'date', 'time', 'event']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writerow({'userid': userid, 'date': date, 'time': time})
+        writer.writerow({'userid': userid, 'date': date, 'time': time, 'event': event})
 
 
 def del_event(userid, e, d, t):
